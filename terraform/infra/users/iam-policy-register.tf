8f3031a1132fd08c11947806d154707e6b6cc65c
Rafael <raferdev@outlook.com>
2024-05-11 19:02:59 -0300

    feat(terraform): add cloudwatch permission

--- terraform/infra/users/iam-policy-register.tf
+++ terraform/infra/users/iam-policy-register.tf
@@ -11,7 +11,16 @@ resource "aws_iam_policy" "register_policy" {
        ],
         "Effect": "Allow",
        "Resource": "${aws_dynamodb_table.users.arn}"
-     }
+     },
+     {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+        "Resource": "*"
+    }
    ]
 } 
 EOF
