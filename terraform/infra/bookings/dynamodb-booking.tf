26997c934c43377ab20d47af93c44b916bf3e673
Rafael <raferdev@outlook.com>
2024-05-12 00:11:38 -0300

    feat(terraform): create new booking policy

---
+++ terraform/infra/bookings/dynamodb-booking.tf
@@ -0,0 +1,19 @@
+resource "aws_dynamodb_table" "bookings" {
+  name = "${var.environment}-bookings"
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+  
+    write_capacity = "${var.write_capacity}"
+    read_capacity = "${var.read_capacity}"
+
+}
+
+resource "aws_ssm_parameter" "aws_dynamodb_bookings_table" {
+    name = "${var.environment}-dynamodb-bookings-table"
+    type = "String"
+    value = "${aws_dynamodb_table.bookings.name}"
+}
