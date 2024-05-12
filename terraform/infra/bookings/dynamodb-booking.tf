5add98b1d046391633918fe3e6578424e61ebba0
Rafael <raferdev@outlook.com>
2024-05-12 18:22:45 -0300

    feat(terraform): add new stream to bookings table

--- terraform/infra/bookings/dynamodb-booking.tf
+++ terraform/infra/bookings/dynamodb-booking.tf
@@ -6,7 +6,9 @@ resource "aws_dynamodb_table" "bookings" {
     name = "id"
     type = "S"
   }
-  
+    stream_enabled = true
+    stream_view_type = "NEW_IMAGE"
+    
     write_capacity = "${var.write_capacity}"
     read_capacity = "${var.read_capacity}"
 
@@ -16,4 +18,10 @@ resource "aws_ssm_parameter" "aws_dynamodb_bookings_table" {
     name = "${var.environment}-dynamodb-bookings-table"
     type = "String"
     value = "${aws_dynamodb_table.bookings.name}"
+}
+
+resource "aws_ssm_parameter" "aws_dynamodb_bookings_stream" {
+    name = "${var.environment}-dynamodb-bookings-stream"
+    type = "String"
+    value = "${aws_dynamodb_table.bookings.stream_arn}"
 }
