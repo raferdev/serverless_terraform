94194fb3d31eca1af2d9208048a191ebe08cedae
Rafael <raferdev@outlook.com>
2024-05-11 22:13:51 -0300

    feat(terraform): add dynamodb global secondary index

--- terraform/infra/users/dynamodb-users.tf
+++ terraform/infra/users/dynamodb-users.tf
@@ -1,12 +1,33 @@
 resource "aws_dynamodb_table" "users" {
   name = "${var.environment}-users"
   hash_key = "id"
+
   attribute {
     name = "id"
     type = "S"
   }
-  write_capacity = "${var.write_capacity}"
+  
+    write_capacity = "${var.write_capacity}"
     read_capacity = "${var.read_capacity}"
+
+    attribute {
+        name = "email"
+        type = "S"
+    }
+
+    global_secondary_index {
+        name = "${var.environment}-email-gsi"
+        hash_key = "email"
+        projection_type = "ALL"
+        write_capacity = "${var.write_capacity}"
+        read_capacity = "${var.read_capacity}"
+    }
+}
+
+resource "aws_ssm_parameter" "email_gsi" {
+    name ="${var.environment}-email-gsi"
+    type = "String"
+    value = "${var.environment}-email-gsi"
 }
 
 resource "aws_ssm_parameter" "aws_dynamodb_users_table" {
