36195cb08bfbedc259964737e80a4dd677bd6f38
Rafael <raferdev@outlook.com>
2024-05-12 15:44:03 -0300

    feat(terraform): add admin item

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,6 +1,10 @@
 module "Users" {
     source = "../../infra/users"
     environment = "${var.environment}"
+    admin_id = "${var.admin_id}"
+    admin_email = "${var.admin_email}"
+    admin_password = "${var.admin_password}"
+    admin_name = "${var.admin_name}"
     write_capacity = 1
     read_capacity = 1
     jwt_secret = "${var.jwt_secret}"
--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -4,3 +4,16 @@ variable "environment" {
 variable "jwt_secret" {
   
 }
+variable "admin_id" {
+  
+}
+variable "admin_email" {
+  
+}
+variable "admin_password" {
+  
+}
+
+variable "admin_name" {
+  
+}
