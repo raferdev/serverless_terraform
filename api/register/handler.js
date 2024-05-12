058d2e5463e29d976b3ebb789cc1fe362a3fa7e6
Rafael <raferdev@outlook.com>
2024-05-11 21:37:58 -0300

    feat(api): put user register

--- api/register/handler.js
+++ api/register/handler.js
@@ -1,18 +1,28 @@
 'use strict';
 
-module.exports.register = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Go Serverless v1.0! Your function executed successfully : ${process.env.DYNAMODB_USERS}!`,
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+import {v4 as uuid} from 'uuid'
+import bcrypt from 'bcryptjs'
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+const register = async (event) => {
+  const body = JSON.parse(event.body);
+
+  const command = new PutCommand({
+    TableName: process.env.DYNAMODB_USERS,
+    Item: {
+      id: uuid(),
+      name: body.name,
+      email: body.email,
+      password: bcrypt.hashSync(body.password, 10),
+    },
+  });
+
+  const response = await docClient.send(command);
+  return response
 };
+
+export {register}
