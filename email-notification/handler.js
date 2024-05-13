37e8ad9b1dcd59db46eb03fd51f27050b3880bb2
Rafael <raferdev@outlook.com>
2024-05-13 03:06:46 -0300

    feat(email-notification): new email notification lambda function

--- email-notification/handler.js
+++ email-notification/handler.js
@@ -1,6 +1,31 @@
-'use strict';
+import nodemailer from 'nodemailer';
 
-module.exports.send = async (event) => {
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_SERVER,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_FROM,
+    pass: process.env.EMAIL_FROM_PASSWORD,
+  },
+});
+console.log(process.env.EMAIL_FROM, process.env.EMAIL_FROM_PASSWORD, process.env.EMAIL_TO, process.env.SMTP_SERVER)
 
-  return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+const send = async (event) => {
+  const emailPromises = []
+  for (const record of event.Records) {
+   const message =  JSON.parse(record.body).Message
+
+ emailPromises.push(  transporter.sendMail({
+    from: `"Bookings 👻" <${process.env.EMAIL_FROM}>`,
+    to: process.env.EMAIL_TO, 
+    subject: "Booking confirmed ✔", 
+    text: message,
+    html: message,
+  }))
+  }
+  await Promise.all(emailPromises)  
+  console.log('Emails sent')
+}
+
+export {send}
