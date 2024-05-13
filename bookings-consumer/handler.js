cf1547941724bbc89bd673d4a9ff17de65231467
Rafael <raferdev@outlook.com>
2024-05-13 00:31:17 -0300

    feat(terraform): add sqs email resource

--- bookings-consumer/handler.js
+++ bookings-consumer/handler.js
@@ -1,14 +1,28 @@
 import { unmarshall }  from "@aws-sdk/util-dynamodb"
 import moment from 'moment'
-const listen = async (event) => {
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 
+const client = new SNSClient({ region: "REGION" });
+
+
+const listen = async (event) =>
+  {
   for (const record of event.Records) {
+    const snsPromises = []
     const { eventName, dynamodb } = record;
     if (eventName === 'INSERT') {
       const { NewImage } = dynamodb;
       const booking = unmarshall(NewImage);
-      console.log('New booking', moment(booking.date).format('llll'));
+      const params = {
+        TopicArn: process.env.SNS_NOTIFICATIONS_ARN,
+        Message: `New booking on ${moment(booking.date).format('llll')}`,
+      }
+      const command =  PublishCommand(params)
+      snsPromises.push(client.send(command))
     }
+    await Promise.all(snsPromises)
+    console.log('New bookings')
+
   }
 
 };
